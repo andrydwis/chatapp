d3d8a436f01b0ef355ded7838e943c1ed063fa77
Salawat Dedaunan <andry.dwi.s@gmail.com>
2020-12-11 18:58:48 +0700

    add time

--- resources/views/livewire/chat-list.blade.php
+++ resources/views/livewire/chat-list.blade.php
@@ -1,6 +1,9 @@
 <div>
     @foreach($messages as $message)
-    <div class="card">
+    <div class="card mb-1">
+        <div class="card-header">
+            {{$message->created_at->diffForHumans()}}
+        </div>
         <div class="card-body">
             {{$message->content}}
         </div>
